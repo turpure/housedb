225ac7e7373259e1a5e6eb264adb14ddfc88b67f
turpure <p812816627@163.com>
2018-03-06 14:07:38 +0800

    test proccess task

--- src/main/java/com/github/coolcool/sloth/lianjiadb/timetask/TestTimeTask.java
+++ src/main/java/com/github/coolcool/sloth/lianjiadb/timetask/TestTimeTask.java
@@ -1,30 +1,37 @@
 package com.github.coolcool.sloth.lianjiadb.timetask;
 
 import com.github.coolcool.sloth.lianjiadb.common.MyHttpClient;
+import com.github.coolcool.sloth.lianjiadb.service.ProcessService;
 import com.github.coolcool.sloth.lianjiadb.service.impl.support.LianjiaWebUtil;
+import com.github.coolcool.sloth.lianjiadb.service.impl.ProcessServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+import org.springframework.stereotype.Service;
 import java.util.Calendar;
+import java.util.TimerTask;
 
 @Component
 @EnableScheduling
-public class TestTimeTask {
+@Service
+public class TestTimeTask{
+
+    @Autowired
+    private ProcessService processService;
 
     private static final Logger log = LoggerFactory.getLogger(FetchHouseIndexTimeTask.class);
 
     /**
      * 根据当天的执行任务，按最小区域（车陂、华景）分页获取房屋链接地址，入库 houseindex
      */
-    @Scheduled(cron="0/20 * * * * ?")   //每5分钟执行一次
+    @Scheduled(cron="0 0/5 * * * ?")   //每5分钟执行一次
     public void exe() {
         log.info("开始执行houseUrlsFetching...");
         try {
-           String url = LianjiaWebUtil.getCityAreaIndexUrl("sh","pudong");
-            log.info(url);
+            processService.fetchHouseUrls();
             }catch (Throwable t){
                 t.printStackTrace();
             }
